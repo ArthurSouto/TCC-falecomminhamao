8f7b86e59e5852c12666353f193bf67f5ae84f8e
ArthurSouto <arthursouto2005@gmail.com>
2022-03-14 16:20:34 -0300

    editando a home.php

---
+++ resources/views/home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
+        </style>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased" style="text-align:justify">
+
+    <div class="Introducao">
+        <h2>Introdução à Libras</h2>
+            <p>Aprenda a utilizar a lingua brasileira de sinais(Libras) de forma contextualizada adiquirindo o vocabuulario para garantir o atendimento e tratamento</p>
+             adequado as pessoas com deficiena auditiva.</p>
+            A lei n°10.436/2002 legitima a Libras como idioma advindo das comunidades surdas brasileiras e obriga o poder publico a  adotar formas</p>
+            institucionalizadas a apoiar o uso e a propagação da lingua como meio de comuniação.
+            </p>
+    </div>
+
+    <div class="Sobre" style="text-align:justify">
+            <h1>SOBRE O CURSO</h1>
+                <p>Nivel de experiencia: Básico</p>
+                <p>Publico alvo: Qualquer pessoa que tenha interesse em aprender a Língua Brasileira de Sinais – Libras.</p>
+                <p>Carga horaria: 24 horas</p>
+    </div>
+
+            <div class="Conteudo" style="text-align:justify">
+                <h3>Conteudo</h3>
+                    <div class="Unidade 1">
+                    <dt><h3>Unidade 1</h3></dt>
+                        <dd>Alfabeto e Datilogia em libras</dd>
+                        <dd>Sinais e Pronomes Pessoais</dd>
+                        <dd>Saudações/Cumprimentos</dd>
+                        <dd>Sinais de Numerais</dd>
+                        <dd>Diálogos</dd>
+                    </div>
+                    <div class="Unidade 2">
+                        <dt><h3>Unidade 2</h3></dt>
+                        <dd>Sinais de calendario e dias da semana em libras </dd>
+                        <dd>Sinais de Advérbios de tempo</dd>
+                        <dd>Sinais de Expressões faciais e tipos de frases em libras</dd>
+                        <dd>Diálogos</dd>
+                    </div>
+                    <div class="Unidade 3">
+                        <dt><h3>Unidade 3</h3></dt>
+                        <dd>Gramática</dd>
+                        <dd>Sinais de relações de parentesco/família</dd>
+                    </div>
+                    <div class="Unidade 4">
+                        <dt><h3>Unidade 4</h3></dt>
+                        <dd>Sinais de sentimentos e Antônomias</dd>
+                    </div>
+                    <div class="Unidade 5">
+                        <dt><h3>Unidade 5</h3></dt>
+                        <dd>Valores monetários</dd>
+                        <dd>Sinais de Operações Aritméticos</dd>
+                    </div>
+                    <div class="Unidade 6">
+                        <dt><h3>Unidade 6</h3></dt>
+                        <dd>Sinais de cores e adjetivos</dd>
+                    </div>
+                    <div class="Unidade 7">
+                        <dt><h3>Unidade 7</h3></dt>
+                        <dd>Sinais de transporte</dd>
+                        <dd>Sinais de comuniação</dd>
+                        <dd>Sinais de materiasi escolares</dd>
+                    </div>
+                    <div class="Unidade 8">
+                        <dt><h3>Unidade 8</h3></dt>
+                        <dd>Sinais de estados do Brasil</dd>
+                        <dd>Sinais de pontos turisticos</dd>
+                    </div>
+                    <div class="Unidade 9">
+                        <dt><h3>Unidade 9</h3></dt>
+                        <dd>Sinais de alimentos</dd>
+                    </div>
+                    <div class="Unidade10">
+                        <dt><h3>Unidade 10</h3></dt>
+                        <dd>Sinais de animais</dd>
+                    </div>
+                    <div class="Unidade 11">
+                        <dt><h3>Unidade 11</h3></dt>
+                        <dd>Sinais de saúde</dd>
+                    </div>
+            </div>
+    </body>
+</html>
